f5b06055bf3b3507f04edd1c417f07ab238cfcf5
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-16 09:07:25 -0500

    updated update function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { getAllTasks, addTask, updateTask } from "./src/tasks.js"
+import { getAllTasks, addTask, updateTask, deleteTask } from "./src/tasks.js"
 
 const app = express()
 app.use(cors())
@@ -10,5 +10,6 @@ app.use(express.json())
 app.get('/tasks', getAllTasks)
 app.post('/tasks', addTask)
 app.patch('/tasks/:taskId', updateTask)
+app.delete('/deleteTask/:taskId', deleteTask)
 
 export const api = functions.https.onRequest(app)
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -3,8 +3,9 @@ import { FieldValue } from "firebase-admin/firestore";
 
 export async function getAllTasks(req, res) {
   const db = await getFirestoreInstance();
+
   db.collection('tasks').orderBy('createdAt', 'desc').get()
-  .then(collection => {
+    .then(collection => {
       const tasks = collection.docs.map(doc => ({ taskId: doc.id, ...doc.data() }));
       res.send(tasks);
     })
@@ -15,15 +16,26 @@ export async function addTask(req, res) {
   const { task } = req.body;
   const newTask = { task, createdAt: FieldValue.serverTimestamp() }
   const db = await getFirestoreInstance();
+
   db.collection('tasks').add(newTask)
     .then(() => getAllTasks(req, res))
     .catch(err => res.status(500).send({ error: err.message }));
 }
 
-export async function updateTask(req, res) {
-  const { done } = req.body;
+export async function deleteTask(req, res) {
   const { taskId } = req.params;
   const db = await getFirestoreInstance();
+
+  db.collection('tasks').doc(taskId).delete()
+    .then(() => getAllTasks(req, res))
+    .catch(err => res.status(500).send({ error: err.message }));
+}
+
+export async function updateTask(req, res) {
+  const { taskId } = req.params;
+  const { done } = req.body;
+  const db = await getFirestoreInstance();
+
   db.collection('tasks').doc(taskId).update({ done })
     .then(() => getAllTasks(req, res))
     .catch(err => res.status(500).send({ error: err.message }));
